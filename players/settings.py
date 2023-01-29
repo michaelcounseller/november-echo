472a94ec9a0a99cf5aeaf92ac8f49c34ac3b64c0
michaelcounseller <michaelcounseller@gmail.com>
2023-01-28 20:38:52 -0600

    changing cors origin list

--- players/settings.py
+++ players/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5173']
+CORS_ALLOWED_ORIGINS = ['http://45.32.95.133']
 
 ROOT_URLCONF = 'players.urls'
 
