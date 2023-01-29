74d2665ed0fa2aa47e9ea01d424ae8df75c3648f
michaelcounseller <michaelcounseller@gmail.com>
2023-01-29 14:43:35 -0600

    adding build

--- players/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Player(models.Model):
-    name = models.CharField(max_length=16)
-    team = models.CharField(max_length=200)
--- players/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from players.models import Player
-
-class PlayerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Player
-        fields = '__all__'
